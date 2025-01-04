197625a52f553b3fb9ef837e490599913eae6103
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-01-04 14:03:21 +0100

    add section 3

--- section3/src/main/java/org/yascode/section3/config/ProjectSecurityConfig.java
+++ section3/src/main/java/org/yascode/section3/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package org.yascode.section3.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,4 +26,20 @@ public class ProjectSecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}yascode")
+                .authorities("read")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$aE7ej/tUVJYbYPM3xxkYF.8TNID..jXBQ5Mui03AiROWvQYs/kVfy")
+                .authorities("admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
