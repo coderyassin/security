8a878adc0498dccd20bc701fbe81d289b0b96623
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-01-05 19:32:02 +0100

    add section 4

--- section3/src/main/java/org/yascode/section3/config/ProjectSecurityConfig.java
+++ section3/src/main/java/org/yascode/section3/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package org.yascode.section3.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -36,7 +38,7 @@ public class ProjectSecurityConfig {
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$aE7ej/tUVJYbYPM3xxkYF.8TNID..jXBQ5Mui03AiROWvQYs/kVfy")
+                .password("{bcrypt}$2a$12$0nOXmsjTvLuxCwAk2217Qe5sV30RnLrHXhHsgYt3QTZjXYEKSBJaK")
                 .authorities("admin")
                 .build();
 
@@ -46,5 +48,10 @@ public class ProjectSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
