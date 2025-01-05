8decaa025dd45feae8c9631c780d1b006ae74946
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-01-05 11:23:24 +0100

    add section 3

--- section3/src/main/java/org/yascode/section3/config/ProjectSecurityConfig.java
+++ section3/src/main/java/org/yascode/section3/config/ProjectSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -41,4 +43,8 @@ public class ProjectSecurityConfig {
         return new InMemoryUserDetailsManager(user, admin);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
