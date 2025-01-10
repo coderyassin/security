761a9cd7aa234d2648fd412f4a17290dccecacd4
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-01-10 17:32:24 +0100

    add section 6

---
+++ section6/src/main/java/org/yascode/section6/config/MagicUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,35 @@
+package org.yascode.section6.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.yascode.section6.exception.NotAMagicUserException;
+
+import java.util.List;
+
+@Component
+@Profile("!prod")
+@Slf4j
+public class MagicUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        if(username.equals("magic")) {
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("magic"));
+            return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+        }
+        throw new NotAMagicUserException(username + " is not a magic user");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
---
+++ section6/src/main/java/org/yascode/section6/exception/NotAMagicUserException.java
@@ -0,0 +1,14 @@
+package org.yascode.section6.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class NotAMagicUserException extends AuthenticationException {
+
+    public NotAMagicUserException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public NotAMagicUserException(String msg) {
+        super(msg);
+    }
+}
