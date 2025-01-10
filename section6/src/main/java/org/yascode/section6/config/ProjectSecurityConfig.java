33cda2586aebeabab667c192442976eb8263bcbc
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-01-10 16:47:34 +0100

    add section 6

--- section6/src/main/java/org/yascode/section6/config/ProjectSecurityConfig.java
+++ section6/src/main/java/org/yascode/section6/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.yascode.section6.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,6 +19,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @Profile("!prod")
+@RequiredArgsConstructor
 public class ProjectSecurityConfig {
 
     @Bean
