908cd7565def1b664439d38e56d1e515e6b95227
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2025-01-10 17:03:16 +0100

    add section 6

---
+++ section6/src/main/java/org/yascode/section6/config/EazyBankProdUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,125 @@
+package org.yascode.section6.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.SpringSecurityMessageSource;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+@Profile("prod")
+@RequiredArgsConstructor
+@Slf4j
+public class EazyBankProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    protected MessageSourceAccessor messages = SpringSecurityMessageSource.getAccessor();
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        Assert.isInstanceOf(UsernamePasswordAuthenticationToken.class, authentication,
+                () -> this.messages.getMessage("AbstractUserDetailsAuthenticationProvider.onlySupports",
+                        "Only UsernamePasswordAuthenticationToken is supported"));
+        String username = determineUsername(authentication);
+        UserDetails user;
+        try {
+            user = retrieveUser(username, (UsernamePasswordAuthenticationToken) authentication);
+        }
+        catch (UsernameNotFoundException ex) {
+            log.debug("Failed to find user '{}'", username);
+            throw new BadCredentialsException(this.messages
+                    .getMessage("AbstractUserDetailsAuthenticationProvider.badCredentials", "Bad credentials"));
+        }
+        Assert.notNull(user, "retrieveUser returned null - a violation of the interface contract");
+        try {
+            preAuthenticationChecks(user);
+            additionalAuthenticationChecks(user, (UsernamePasswordAuthenticationToken) authentication);
+        }
+        catch (AuthenticationException ex) {
+            throw ex;
+        }
+        postAuthenticationChecks(user);
+
+        Object principalToReturn = user;
+
+        return createSuccessAuthentication(principalToReturn, authentication, user);
+    }
+
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+    private String determineUsername(Authentication authentication) {
+        return (authentication.getPrincipal() == null) ? "NONE_PROVIDED" : authentication.getName();
+    }
+
+    private UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication)
+            throws AuthenticationException {
+        try {
+            UserDetails loadedUser = this.userDetailsService.loadUserByUsername(username);
+            if (loadedUser == null) {
+                throw new InternalAuthenticationServiceException(
+                        "UserDetailsService returned null, which is an interface contract violation");
+            }
+            return loadedUser;
+        }
+        catch (UsernameNotFoundException ex) {
+            throw ex;
+        }
+        catch (InternalAuthenticationServiceException ex) {
+            throw ex;
+        }
+        catch (Exception ex) {
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex);
+        }
+    }
+
+    private void preAuthenticationChecks(UserDetails user) {
+    }
+
+    private void additionalAuthenticationChecks(UserDetails userDetails,
+                                                UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        if (authentication.getCredentials() == null) {
+            log.debug("Failed to authenticate since no credentials provided");
+            throw new BadCredentialsException(this.messages
+                    .getMessage("AbstractUserDetailsAuthenticationProvider.badCredentials", "Bad credentials"));
+        }
+        String presentedPassword = authentication.getCredentials().toString();
+        if (!this.passwordEncoder.matches(presentedPassword, userDetails.getPassword())) {
+            log.debug("Failed to authenticate since password does not match stored value");
+            throw new BadCredentialsException(this.messages
+                    .getMessage("AbstractUserDetailsAuthenticationProvider.badCredentials", "Bad credentials"));
+        }
+    }
+
+    private void postAuthenticationChecks(UserDetails user) {
+
+    }
+
+    protected Authentication createSuccessAuthentication(Object principal, Authentication authentication,
+                                                         UserDetails user) {
+        UsernamePasswordAuthenticationToken result = UsernamePasswordAuthenticationToken.authenticated(principal,
+                authentication.getCredentials(), this.authoritiesMapper.mapAuthorities(user.getAuthorities()));
+        result.setDetails(authentication.getDetails());
+        log.debug("Authenticated user");
+        return result;
+    }
+}
--- section6/src/main/java/org/yascode/section6/config/ProjectSecurityProdConfig.java
+++ section6/src/main/java/org/yascode/section6/config/ProjectSecurityProdConfig.java
@@ -1,4 +1,4 @@
-package org.yascode.section6.config.prod;
+package org.yascode.section6.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
